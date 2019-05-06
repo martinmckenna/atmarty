c8eb864fd258551a5af684439537525c653c9cc0
mmckenna <mmckenna.phila@gmail.com>
2019-05-05 20:23:57 -0400

    adds cors support for email

--- email.php
+++ email.php
@@ -7,8 +7,34 @@ require './vendor/autoload.php';
 // ini_set('display_startup_errors', 1);
 // error_reporting(E_ALL);
 
-header('Access-Control-Allow-Origin: *');
-header('Content-Type: application/json');
+$origin = $_SERVER['HTTP_ORIGIN'];
+$allowed_domains = [
+    'http://localhost:3000',
+    'https://www.atmarty.com',
+    'http://www.barcart.net',
+];
+
+if (in_array($origin, $allowed_domains)) {
+    header('Access-Control-Allow-Origin: ' . $origin);
+}
+
+// Allow from any origin
+if (isset($_SERVER['HTTP_ORIGIN'])) {
+  header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
+  header('Access-Control-Allow-Credentials: true');
+  header('Access-Control-Max-Age: 86400');    // cache for 1 day
+}
+
+  // Access-Control headers are received during OPTIONS requests
+if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+  if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD']))
+      header("Access-Control-Allow-Methods: GET, POST, OPTIONS");         
+
+  if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']))
+      header("Access-Control-Allow-Headers:        {$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");
+
+  exit(0);
+}
 
 $content = trim(file_get_contents("php://input"));
 $content = json_decode($content);
